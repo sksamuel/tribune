0293e2c14cf7dcf17e7771d6189c4e84ba34b497
sksamuel <sam@sksamuel.com>
2017-12-17 11:53:25 +0000

    Added sanitization support

--- src/main/scala/com/sksamuel/monkeytail/ValidatorSyntax.scala
+++ src/main/scala/com/sksamuel/monkeytail/ValidatorSyntax.scala
@@ -8,10 +8,12 @@ import scala.language.experimental.macros
 object ValidatorSyntax {
 
   // enables the use of the magic macro methods on a validator
-  implicit class ValidatorOps[T](val wrapped: Validator[T]) {
+  implicit class ValidatorOps[T <: Product](val wrapped: Validator[T]) {
 
     // prepares a field to be used as the basis for a validator
+    @deprecated("use validate(T => U)", "0.91.0")
     def field[U](extractor: T => U): FieldContext[T, U] = macro Macros.fieldContext[T, U]
+    def validate[U](extractor: T => U): FieldContext[T, U] = macro Macros.fieldContext[T, U]
 
     // prepares an iterable field to be used as a validator
     def forall[U](extractor: T => Seq[U]): SeqContext[T, U] = macro Macros.seqContext[T, U]
@@ -20,6 +22,9 @@ object ValidatorSyntax {
       val delegate = Validator.seq[T, U](extractor, validator)
       Validator.monoid[T].combine(wrapped, delegate)
     }
+
+    // prepares a field to be used for sanitization
+    def sanitize[U](extractor: T => U): SanitizeContext[T, U] = macro Macros.sanitizeContext[T, U]
 
     // uses an existing validator for the extracted field
     def valid[U](extractor: T => U)(implicit validator: Validator[U]): Validator[T] = {
@@ -40,34 +45,5 @@ object ValidatorSyntax {
       }
       Validator.monoid[T].combine(wrapped, validator)
     }
-  }
-}
-
-case class SeqContext[T, U](extractor: T => Seq[U],
-                            parent: Validator[T], // the current validator we'll be combining with
-                            path: Path) {
-
-  def apply(test: U => Boolean)(implicit builder: ViolationBuilder[U] = DefaultViolationBuilder): Validator[T] = {
-    val validator = Validator.seq[T, U](extractor, new DefaultValidator[U](test, builder))
-    Validator.monoid[T].combine(parent, validator)
-  }
-}
-
-case class FieldContext[T, U](extractor: T => U, // function to retrieve the field we want to test
-                              parent: Validator[T], // the current validator we'll be combining with
-                              path: Path // the path to the field we're evaluating
-                             ) {
-
-  def apply(test: U => Boolean)(implicit builder: ViolationBuilder[U] = DefaultViolationBuilder): Validator[T] = {
-    val validator = new Validator[T] {
-      override def apply(t: T): Validated[NonEmptyList[Violation], T] = {
-        val value = extractor(t)
-        if (test(value)) Valid(t) else {
-          val violation = builder(path, value)
-          Invalid(NonEmptyList.of(violation))
-        }
-      }
-    }
-    Validator.monoid[T].combine(parent, validator)
   }
 }
