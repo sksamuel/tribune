55d4d3183a0d169a35b5bc3792aecefc507a2ac7
sksamuel <sam@sksamuel.com>
2022-02-19 08:15:39 -0600

    Added parser combinators to arity 8

--- monkeytail-core/src/jvmMain/kotlin/com/sksamuel/monkeytail/core/parsers/parser.kt
+++ monkeytail-core/src/jvmMain/kotlin/com/sksamuel/monkeytail/core/parsers/parser.kt
@@ -2,6 +2,11 @@ package com.sksamuel.monkeytail.core.parsers
 
 import arrow.core.Nel
 import arrow.core.NonEmptyList
+import arrow.core.Tuple4
+import arrow.core.Tuple5
+import arrow.core.Tuple6
+import arrow.core.Tuple7
+import arrow.core.Tuple8
 import arrow.core.Validated
 import arrow.core.ValidatedNel
 import arrow.core.invalidNel
@@ -36,6 +41,90 @@ fun interface Parser<in I, out A, out E> {
          p3: Parser<INPUT, C, ERROR>,
       ): Parser<INPUT, Triple<A, B, C>, ERROR> = Parser { input ->
          p1.parse(input).zip(p2.parse(input), p3.parse(input)) { a, b, c -> Triple(a, b, c) }
+      }
+
+      fun <INPUT, A, B, C, D, ERROR> compose(
+         p1: Parser<INPUT, A, ERROR>,
+         p2: Parser<INPUT, B, ERROR>,
+         p3: Parser<INPUT, C, ERROR>,
+         p4: Parser<INPUT, D, ERROR>,
+      ): Parser<INPUT, Tuple4<A, B, C, D>, ERROR> = Parser { input ->
+         p1.parse(input).zip(p2.parse(input), p3.parse(input), p4.parse(input)) { a, b, c, d -> Tuple4(a, b, c, d) }
+      }
+
+      fun <INPUT, A, B, C, D, E, ERROR> compose(
+         p1: Parser<INPUT, A, ERROR>,
+         p2: Parser<INPUT, B, ERROR>,
+         p3: Parser<INPUT, C, ERROR>,
+         p4: Parser<INPUT, D, ERROR>,
+         p5: Parser<INPUT, E, ERROR>,
+      ): Parser<INPUT, Tuple5<A, B, C, D, E>, ERROR> = Parser { input ->
+         p1.parse(input).zip(p2.parse(input), p3.parse(input), p4.parse(input), p5.parse(input)) { a, b, c, d, e ->
+            Tuple5(a, b, c, d, e)
+         }
+      }
+
+      fun <INPUT, A, B, C, D, E, F, ERROR> compose(
+         p1: Parser<INPUT, A, ERROR>,
+         p2: Parser<INPUT, B, ERROR>,
+         p3: Parser<INPUT, C, ERROR>,
+         p4: Parser<INPUT, D, ERROR>,
+         p5: Parser<INPUT, E, ERROR>,
+         p6: Parser<INPUT, F, ERROR>,
+      ): Parser<INPUT, Tuple6<A, B, C, D, E, F>, ERROR> = Parser { input ->
+         p1.parse(input).zip(
+            p2.parse(input),
+            p3.parse(input),
+            p4.parse(input),
+            p5.parse(input),
+            p6.parse(input)
+         ) { a, b, c, d, e, f ->
+            Tuple6(a, b, c, d, e, f)
+         }
+      }
+
+      fun <INPUT, A, B, C, D, E, F, G, ERROR> compose(
+         p1: Parser<INPUT, A, ERROR>,
+         p2: Parser<INPUT, B, ERROR>,
+         p3: Parser<INPUT, C, ERROR>,
+         p4: Parser<INPUT, D, ERROR>,
+         p5: Parser<INPUT, E, ERROR>,
+         p6: Parser<INPUT, F, ERROR>,
+         p7: Parser<INPUT, G, ERROR>,
+      ): Parser<INPUT, Tuple7<A, B, C, D, E, F, G>, ERROR> = Parser { input ->
+         p1.parse(input).zip(
+            p2.parse(input),
+            p3.parse(input),
+            p4.parse(input),
+            p5.parse(input),
+            p6.parse(input),
+            p7.parse(input),
+         ) { a, b, c, d, e, f, g ->
+            Tuple7(a, b, c, d, e, f, g)
+         }
+      }
+
+      fun <INPUT, A, B, C, D, E, F, G, H, ERROR> compose(
+         p1: Parser<INPUT, A, ERROR>,
+         p2: Parser<INPUT, B, ERROR>,
+         p3: Parser<INPUT, C, ERROR>,
+         p4: Parser<INPUT, D, ERROR>,
+         p5: Parser<INPUT, E, ERROR>,
+         p6: Parser<INPUT, F, ERROR>,
+         p7: Parser<INPUT, G, ERROR>,
+         p8: Parser<INPUT, H, ERROR>,
+      ): Parser<INPUT, Tuple8<A, B, C, D, E, F, G, H>, ERROR> = Parser { input ->
+         p1.parse(input).zip(
+            p2.parse(input),
+            p3.parse(input),
+            p4.parse(input),
+            p5.parse(input),
+            p6.parse(input),
+            p7.parse(input),
+            p8.parse(input),
+         ) { a, b, c, d, e, f, g, h ->
+            Tuple8(a, b, c, d, e, f, g, h)
+         }
       }
    }
 
