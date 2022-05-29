b81df6874f96493e2075019e3b71e85d986f637c
sksamuel <sam@sksamuel.com>
2022-05-28 23:39:44 -0500

    Added sorucesets

--- optio-core/build.gradle.kts
+++ optio-core/build.gradle.kts
@@ -11,11 +11,38 @@ repositories {
 kotlin {
 
    targets {
-      jvm()
-      js {
+      jvm {
+         compilations.all {
+            kotlinOptions {
+               jvmTarget = "11"
+            }
+         }
+      }
+      js(BOTH) {
          browser()
          nodejs()
       }
+
+      linuxX64()
+
+      mingwX64()
+
+      macosX64()
+      macosArm64()
+
+      tvos()
+      tvosSimulatorArm64()
+
+      watchosArm32()
+      watchosArm64()
+      watchosX86()
+      watchosX64()
+      watchosSimulatorArm64()
+
+      iosX64()
+      iosArm64()
+      iosArm32()
+      iosSimulatorArm64()
    }
 
    sourceSets {
@@ -38,6 +65,76 @@ kotlin {
             implementation(Testing.kotest.assertions.core)
             implementation(Testing.kotest.runner.junit5)
          }
+      }
+
+      val commonTest by getting {
+         dependencies {
+            implementation(project(Projects.Framework.engine))
+         }
+      }
+
+      val desktopMain by creating {
+         dependsOn(commonMain)
+      }
+
+      val macosX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val macosArm64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val mingwX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val linuxX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val iosX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val iosArm64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val iosArm32Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val iosSimulatorArm64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val watchosArm32Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val watchosArm64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val watchosX86Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val watchosX64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val watchosSimulatorArm64Main by getting {
+         dependsOn(desktopMain)
+      }
+
+      val tvosMain by getting {
+         dependsOn(desktopMain)
+      }
+
+      val tvosSimulatorArm64Main by getting {
+         dependsOn(desktopMain)
       }
 
       all {
