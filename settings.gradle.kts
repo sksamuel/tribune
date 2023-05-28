b60fe32f7bd67165a16b6480a475cdc58ba0f725
sksamuel <sam@sksamuel.com>
2023-05-28 13:26:55 -0500

    Removed refresh versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,18 +12,8 @@ include(
 pluginManagement {
    plugins {
       id("org.springframework.boot") version ("2.7.12")
-   }
-}
-
-plugins {
-   // See https://jmfayard.github.io/refreshVersions
-   id("de.fayard.refreshVersions") version "0.40.2"
-}
-
-refreshVersions {
-   enableBuildSrcLibs()
-   rejectVersionIf {
-      candidate.stabilityLevel != de.fayard.refreshVersions.core.StabilityLevel.Stable
+      kotlin("multiplatform") version ("1.8.21")
+      kotlin("plugin.spring") version ("1.8.21")
    }
 }
 
