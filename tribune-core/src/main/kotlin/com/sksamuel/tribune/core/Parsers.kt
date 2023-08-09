0f265805583df39aa5c1e755e86e71221db304b0
sksamuel <sam@sksamuel.com>
2023-08-09 09:01:03 -0500

    Added Parsers.notNull()

--- tribune-core/src/main/kotlin/com/sksamuel/tribune/core/Parsers.kt
+++ tribune-core/src/main/kotlin/com/sksamuel/tribune/core/Parsers.kt
@@ -6,6 +6,8 @@ object Parsers {
 
    val nullableString: Parser<String?, String?, Nothing> = Parser.fromNullableString()
 
+   fun <I, E> notNull(error: () -> E): Parser<I?, I, E> = Parser.invoke<I?>().notNull { error() }
+
    fun <E> nonBlankString(error: () -> E): Parser<String?, String, E> =
       Parser.from<String?>().notNullOrBlank { error() }
 }
