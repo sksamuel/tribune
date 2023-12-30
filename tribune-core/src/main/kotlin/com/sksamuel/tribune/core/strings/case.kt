7ed0594cad05f8430eb768e4f6d64303da2274bb
sksamuel <sam@sksamuel.com>
2023-12-30 10:28:32 -0600

    Added upper/lower case String? variants

--- tribune-core/src/main/kotlin/com/sksamuel/tribune/core/strings/case.kt
+++ tribune-core/src/main/kotlin/com/sksamuel/tribune/core/strings/case.kt
@@ -2,17 +2,34 @@ package com.sksamuel.tribune.core.strings
 
 import com.sksamuel.tribune.core.Parser
 import com.sksamuel.tribune.core.map
+import com.sksamuel.tribune.core.mapIfNotNull
 
 /**
- * Modifies a String -> String [Parser] by uppercasing the output string.
+ * Modifies an I -> String [Parser] by uppercasing the output string.
  *
  * @return the output of the underlying parser with the output uppercased.
  */
 fun <I, E> Parser<I, String, E>.toUppercase(): Parser<I, String, E> = map { it.uppercase() }
 
 /**
- * Modifies a String -> String [Parser] by lowercasing the output string.
+ * Modifies an I -> String? [Parser] by uppercasing the output string.
+ *
+ * @return the output of the underlying parser with the output uppercased.
+ */
+@JvmName("toUppercaseOrNull")
+fun <I, E> Parser<I, String?, E>.toUppercase(): Parser<I, String?, E> = mapIfNotNull { it.uppercase() }
+
+/**
+ * Modifies an I -> String [Parser] by lowercasing the output string.
  *
  * @return the output of the underlying parser with the output lowercased.
  */
 fun <I, E> Parser<I, String, E>.toLowercase(): Parser<I, String, E> = map { it.lowercase() }
+
+/**
+ * Modifies an I -> String? [Parser] by lowercasing the output string.
+ *
+ * @return the output of the underlying parser with the output lowercased.
+ */
+@JvmName("toLowercaseOrNull")
+fun <I, E> Parser<I, String?, E>.toLowercase(): Parser<I, String?, E> = mapIfNotNull { it.lowercase() }
