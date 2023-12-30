c52ba2036d15507c01dfdefd77d304f8e17371d6
sksamuel <sam@sksamuel.com>
2023-12-30 13:59:22 -0600

    Added nullable version of enum

--- tribune-core/src/main/kotlin/com/sksamuel/tribune/core/enums/enums.kt
+++ tribune-core/src/main/kotlin/com/sksamuel/tribune/core/enums/enums.kt
@@ -1,5 +1,6 @@
 package com.sksamuel.tribune.core.enums
 
+import arrow.core.Either
 import arrow.core.leftNel
 import arrow.core.right
 import com.sksamuel.tribune.core.Parser
@@ -12,6 +13,19 @@ import com.sksamuel.tribune.core.flatMap
 inline fun <I, E, reified ENUM : Enum<ENUM>> Parser<I, String, E>.enum(crossinline ifError: (String) -> E): Parser<I, ENUM, E> {
    return flatMap { symbol ->
       runCatching { enumValueOf<ENUM>(symbol) }
+         .fold({ it.right() }, { ifError(symbol).leftNel() })
+   }
+}
+
+/**
+ * Wraps a [Parser] that produces nullable Strings to one that produces nullable enums.
+ * If the String is not a valid enum value, then an error is produced using [ifError].
+ * If the String is null, then null is produced.
+ */
+inline fun <I, E, reified ENUM : Enum<ENUM>> Parser<I, String?, E>.enum(crossinline ifError: (String) -> E): Parser<I, ENUM?, E> {
+   return flatMap { symbol ->
+      if (symbol == null) Either.Right(null)
+      else runCatching { enumValueOf<ENUM>(symbol) }
          .fold({ it.right() }, { ifError(symbol).leftNel() })
    }
 }
